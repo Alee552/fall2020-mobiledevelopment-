df7a1c9071c3fabbc4e15f5a9d8fb98e17505fd2
Alee552 <70787311+Alee552@users.noreply.github.com>
2020-10-05 21:35:16 -0400

    Example added

--- week6/src/App.js
+++ week6/src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Example from './components/Example';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Example name="Alexis Lee"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- week6/src/components/Example/index.js
+++ week6/src/components/Example/index.js
@@ -1,1 +1,8 @@
+import React, { Component } from 'react';
 
+export default class Example extends React.Component {
+
+render() {
+  return (<p> Hi, my name is { this.props.name }. </p>);
+    }
+}
