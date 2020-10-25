48ff249223bc0a17c29d4e541264e3b2764114d2
Alee552 <70787311+Alee552@users.noreply.github.com>
2020-10-24 23:49:18 -0400

    Attempts to add CSS

--- week6/src/components/Example/index.js
+++ week6/src/components/Example/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import React from 'react';
+import   Image from './images/nasaunsplash.jpg'; 
 
 export default class Example extends React.Component {
 
@@ -6,3 +8,24 @@ render() {
   return (<p> Hi, my name is { this.props.name }. </p>);
     }
 }
+
+
+class Example extends React.Component {
+  render() {
+    return (
+      <div>
+      <p>Hello Everyone!</p>
+      </div>
+    );
+  }
+}
+class Example extends React.Component {
+  render() {
+    return (
+      <div>
+      <p style={{color:"lightblue"}}  />
+      </div>
+    );
+  }
+}
+export default App;
