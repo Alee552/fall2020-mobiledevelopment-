dac7727dd0ec594b9b8e1fb5fa724744076a6807
Alee552 <70787311+Alee552@users.noreply.github.com>
2020-12-10 16:41:31 -0500

    new components

--- week16/App.js
+++ week16/App.js
@@ -5,26 +5,31 @@ import { Text, Button } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ButtonExample from './components/Jeans';
+import ButtonCardigans from './components/Cardigans';
+import ButtonWinterBoots from './components/WinterBoots';
 
 
 function HomeScreen(props) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text><h1>Passion Fashions </h1></Text>
  <Button
-        title="Steps"
-        onPress={() => props.navigation.navigate("Steps Tracker" )}
+        title="Jeans"
+        onPress={() => props.navigation.navigate("Jeans" )}
       />
    <Button
-        title="Swim Laps"
-        onPress={() => props.navigation.navigate("Swim Laps Tracker")}
+        title="Cardigans"
+        onPress={() => props.navigation.navigate("Cardigans")}
       />
-        
+      <Button
+        title="WinterBoots"
+        onPress={() => props.navigation.navigate("WinterBoots")}
+      />  
 </View>
 );
 }
 
-function StepsTracker  (props) {
+function JeanScreen (props) {
 return(
   <View>
 <Text>Jeans</Text>
@@ -37,14 +42,58 @@ return(
 );
 }
 
+function CardigansScreen (props) {
+  return(
+    <View>
+  <Text>Cardigans</Text>
+  <ButtonCardigans />
+  <Button 
+    title="Back to Home"
+    onPress={() => props.navigation.navigate('Home')}
+    />
+    </View>
+  );
+  }
+
+  function WinterBootsScreen (props) {
+    return(
+      <View>
+    <Text>WinterBootsScreen</Text>
+    <ButtonWinterBoots/>
+    <Button 
+      title="Back to Home"
+      onPress={() => props.navigation.navigate('Home')}
+      />
+      </View>
+    );
+    }
+
+    function SweaterScreen (props) {
+      return(
+        <View>
+      <Text>WinterBootsScreen</Text>
+      <ButtonWinterBoots/>
+      <Button 
+        title="Back to Home"
+        onPress={() => props.navigation.navigate('Home')}
+        />
+        </View>
+      );
+      }
+  
+
+
 const Stack = createStackNavigator();
+
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Steps Tracker" component={StepsTracker} />
-            <Stack.Screen name="Swim Laps Tracker" component={SwimLapsTracker} />
+        <Stack.Screen name="Jeans" component={JeanScreen} />
+        <Stack.Screen name="Cardigans" component={CardigansScreen} />
+        <Stack.Screen name="WinterBoots" component={WinterBootsScreen} />
+        <Stack.Screen name="Sweater" component={SweaterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
