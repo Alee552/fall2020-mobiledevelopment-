f32741f307902c6db03b4a9eb574185fdd5c2d0f
Alee552 <70787311+Alee552@users.noreply.github.com>
2022-02-16 17:16:22 -0500

    Updates

---
+++ precache-manifest.1421ed402ee68e06dda91fa07bc20544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c08a15d9d00d780fb2843f24b65f6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e9ff579e06670798fb9",
+    "url": "/static/css/main.0c2c4ab8.chunk.css"
+  },
+  {
+    "revision": "c5ac83050b1b5d6918b8",
+    "url": "/static/js/2.3f7e3704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f7e3704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9ff579e06670798fb9",
+    "url": "/static/js/main.4ac0c795.chunk.js"
+  },
+  {
+    "revision": "7a868d3a8f12fe4bdd1a",
+    "url": "/static/js/runtime-main.351b2013.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio1/precache-manifest.2ee11b09c3a3c24e8d783edfbb7103fd.js"
+  "/precache-manifest.1421ed402ee68e06dda91fa07bc20544.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
